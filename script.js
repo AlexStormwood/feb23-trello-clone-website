08a33b6a28a0a179c0041a7b60710e87afc416cb
Alex H <alex.holder@bigfootds.com>
2023-09-02 11:42:03 +1000

    feat: render trelloData

--- script.js
+++ script.js
@@ -6,9 +6,44 @@ console.log(trelloData);
 
 function renderColumns(){
 	let trelloDataRowRootNode = document.getElementById("dataDisplayRow");
-
+	// Removing any stale or old HTML content:
 	trelloDataRowRootNode.innerHTML = "";
 
+	// Generating new HTML content:
+	trelloData.columns.forEach((column) => {
+		console.log(column.name);
+
+		let columnNode = document.createElement("div");
+
+		columnNode.classList.add("trelloColumn");
+
+		// Create content to render column data
+		let columnHeading = document.createElement("h3");
+		columnHeading.innerText = column.name;
+		columnNode.appendChild(columnHeading);
+
+		column.cards.forEach((card) => {
+			// Find the card preview, copy it, save the copy to the variable
+			let newCard = document.getElementById("cardPreview").cloneNode(true);
+
+			if (!card.timestamp || isNaN(card.timestamp)){
+				card.timestamp = Date.now();
+				// Number like 1287653419435
+			}
+			newCard.id = card.timestamp;
+
+			// Find the h3 of the card title and change its content
+			newCard.querySelector(".cardDisplay-title").innerText = card.title;
+			
+			// Same as above but for the paragraph tag
+			newCard.querySelector(".cardDisplay-content").innerText = card.content; 
+
+			// After data is all done, attach card to column
+			columnNode.appendChild(newCard);
+		})
+
+		trelloDataRowRootNode.appendChild(columnNode);
+	});
 }
 
 renderColumns();
